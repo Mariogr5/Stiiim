2f9307432a14f1f7a2bfde8079b2fd64e57eeeff
Mariogr5 <mariogr1001@gmail.com>
2022-12-06 18:20:13 +0100

    first

---
+++ Projekt/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projekt.Models;
+using System.Diagnostics;
+namespace Projekt.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Gra> listagier;
+        public HomeController()
+        {
+            listagier = new List<Gra>()
+            {
+                new Gra(1, "pasjans", "Logiczna", 20, "Fajna gra karciana", "Chess.jpg"),
+                new Gra(2, "Szachy", "Logiczna", 30, "Fajna gra planszowa", "Chess.jpg"),
+                new Gra(3, "pasjans", "Logiczna", 20, "Fajna gra karciana", "pasjans.jfif"),
+                new Gra(4, "pasjans", "Logiczna", 20, "Fajna gra karciana", "pasjans.jfif"),
+                new Gra(5, "pasjans", "Logiczna", 20, "Fajna gra karciana", "pasjans.jfif"),
+                new Gra(5, "pasjans", "Logiczna", 20, "Fajna gra karciana", "pasjans.jfif"),
+                new Gra(5, "pasjans", "Logiczna", 20, "Fajna gra karciana", "pasjans.jfif"),
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View(listagier);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View(listagier);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Projekt/Models/Gra.cs
@@ -0,0 +1,23 @@
+﻿namespace Projekt.Models
+{
+    public class Gra
+    {
+        public Gra(int id, string nazwa, string kategoria, int cena, string opis, string zdjęcie)
+        {
+            this.Id = id;
+            this.Nazwa = nazwa;
+            this.Kategoria = kategoria;
+            this.Cena = cena;
+            this.Opis = opis;
+            this.Zdjęcie = zdjęcie;
+        }
+
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public string Kategoria { get; set; }
+        public int Cena { get; set; }
+        public string Opis { get; set; }
+
+        public string Zdjęcie { get; set; }
+    }
+}
